3cac294596c14f27ab3f8d3d7478db4fb6fd23fb
Michael Rolli <michael.rolli@unibe.ch>
2021-04-30 19:58:01 +0200

    Purge everything old and start from a fresh state
    Removed all old files and started with a fresh module generated by pdk.
    Add gihtub_changelog_generator gem and an empty class slurm.
    
    After the major rewrite is done and merged:
    * tag with v1.0.0
    * rollout v1.0.0 and apply to all nodes
    * drop git history and start a new history with
      * an initial commit
      * tagged with v1.0.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,75 +1,8 @@
-# == Class: slurm
+# @summary A short summary of the purpose of this class
 #
-# Module for provisioning and managing of nodes in a SLURM cluster
-# 
-# This class only makes the default parameters available in a more 
-# secure fashion for later use
+# A description of what this class does
 #
-# === Parameters
-#
-# none
-#
-class slurm (
-  $is_slurm_master     = $slurm::params::is_slurm_master,
-  $is_slurm_worker     = $slurm::params::is_slurm_worker,
-  $is_slurm_db         = $slurm::params::is_slurm_db,
-  $is_slurm_login      = $slurm::params::is_slurm_login,
-  $disable_munge       = $slurm::params::disable_munge,
-  $manage_user_locally = $slurm::params::manage_user_locally,
-  $slurm_user          = $slurm::params::slurm_user,
-  $slurm_user_id       = $slurm::params::slurm_user_id,
-  $slurm_group         = $slurm::params::slurm_group,
-  $slurm_group_id      = $slurm::params::slurm_group_id,
-  $munge_user          = $slurm::params::munge_user,
-  $munge_user_id       = $slurm::params::munge_user_id,
-  $munge_group         = $slurm::params::munge_group,
-  $munge_group_id      = $slurm::params::munge_group_id,
-  $munge_key           = $slurm::params::munge_key,
-) inherits slurm::params {
-
-  if $slurm::is_slurm_master {
-    include slurm::common
-    include slurm::master::install
-    include slurm::master::config
-    include slurm::master::service
-
-    Class['slurm::common'] ->
-    Class['slurm::master::install']->
-    Class['slurm::master::config']->
-    Class['slurm::master::service']
-  }
-
-  if $slurm::is_slurm_worker {
-    include slurm::common
-    include slurm::worker::install
-    include slurm::worker::config
-    include slurm::worker::service
-
-    Class['slurm::common'] ->
-    Class['slurm::worker::install']->
-    Class['slurm::worker::config']->
-    Class['slurm::worker::service']
-  }
-
-  if $slurm::is_slurm_db {
-    include slurm::common
-    include slurm::db::install
-    include slurm::db::config
-    include slurm::db::service
-
-    Class['slurm::common'] ->
-    Class['slurm::db::install']->
-    Class['slurm::db::config']->
-    Class['slurm::db::service']
-  }
-
-  if $slurm::is_slurm_login {
-    include slurm::common
-    include slurm::login::config
-
-    Class['slurm::common'] ->
-    Class['slurm::login::config']
-  }
-
+# @example
+#   include slurm
+class slurm {
 }
-
